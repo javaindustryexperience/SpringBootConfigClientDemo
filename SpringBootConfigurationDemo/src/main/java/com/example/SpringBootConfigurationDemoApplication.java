16a81b9af34dc53a64c8472dac6b2ade9f413934
Jeevan <javaindustryexperience@gmail.com>
2025-02-28 17:23:45 +0530

    yml changes

---
+++ SpringBootConfigurationDemo/src/main/java/com/example/SpringBootConfigurationDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+public class SpringBootConfigurationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigurationDemoApplication.class, args);
+	}
+
+}
